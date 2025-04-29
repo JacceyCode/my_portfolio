4b3d16b04aeb694bc79d23ecf903bced5625207a
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2025-04-29 14:07:40 +0100

    project section update

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,55 +5,12 @@ import {
   CounterItem,
   Experience,
   LogoIcons,
+  Project,
   SocialContact,
   TechStack,
   Testimonial,
   Words,
 } from "../types/interface";
-
-///////////////////////////////
-// import careplus from "/images/care-plus.png";
-// import bank from "/images/bank.png";
-// import seron from "/images/seron.png";
-// import oasisclt from "/images/oasis-client.jpeg";
-// import blog from "/images/blog.webp";
-// import dmarketplace from "/images/dmarketplace.webp";
-// import natours from "/images/natours.webp";
-// import oasismgt from "/images/oasis-mgt.jpeg";
-// import moviebox from "/images/moviebox.webp";
-// import pizza from "/images/pizza.webp";
-// import apple from "/images/apple.png";
-// import jafel from "/images/jafel.png";
-
-////////////////////////////////
-// import html from "/images/logos/html.png";
-// import css from "/images/logos/css.png";
-// import javascript from "/images/logos/javascript.png";
-// import typescript from "/images/logos/typescript.png";
-// import sass from "/images/logos/sass.png";
-// import redux from "/images/logos/redux.png";
-// import nextjs from "/images/logos/nextjs.png";
-// import figma from "/images/logos/figma.png";
-// import jest from "/images/logos/jest.png";
-// import aws from "/images/logos/aws.png";
-// import docker from "/images/logos/docker.png";
-// import appwrite from "/images/logos/appwrite.png";
-// import redis from "/images/logos/redis.png";
-// import tailwind from "/images/logos/tailwind.png";
-// import gitactions from "/images/logos/actions.png";
-// import vscode from "/images/logos/vscode.png";
-// import vercel from "/images/logos/vercel.jpeg";
-// import threejs from "/images/logos/three.png";
-// import shell from "/images/logos/shell.png";
-// import postgresql from "/images/logos/postgres.png";
-// import mysql from "/images/logos/mysql.png";
-// import jira from "/images/logos/jira.png";
-// import zustand from "/images/logos/zustand.png";
-// import gsap from "/images/logos/gsap.png";
-// import supabase from "/images/logos/supabase.png";
-// import java from "/images/logos/java.png";
-// import spring from "/images/logos/spring.png";
-// import junit from "/images/logos/junit.png";
 
 const navLinks = [
   {
@@ -68,10 +25,10 @@ const navLinks = [
     name: "Experience",
     link: "experience",
   },
-  {
-    name: "Testimonials",
-    link: "testimonials",
-  },
+  // {
+  //   name: "Testimonials",
+  //   link: "testimonials",
+  // },
 ];
 
 const words: Words[] = [
@@ -374,94 +331,48 @@ const socialContact: SocialContact[] = [
   },
 ];
 
-const projects = [
-  // {
-  //   name: "Jafel Project",
-  //   description:
-  //     "A Project Management app designed for creating, scheduling, assigning, tracking, and reporting tasks and projects. It helps to ensure project completion within budget and on time by providing tools for planning, resource allocation, and progress monitoring.",
-  //   tags: [
-  //     {
-  //       name: "Next.js",
-  //       color: "orange-text-gradient",
-  //     },
-  //     {
-  //       name: "tailwindcss",
-  //       color: "green-text-gradient",
-  //     },
-  //     {
-  //       name: "typescript",
-  //       color: "blue-text-gradient",
-  //     },
-  //     {
-  //       name: "redux-toolkit",
-  //       color: "pink-text-gradient",
-  //     },
-  //     {
-  //       name: "recharts",
-  //       color: "orange-text-gradient",
-  //     },
-  //     {
-  //       name: "react-dnd",
-  //       color: "orange-text-gradient",
-  //     },
-  //     {
-  //       name: "node.js",
-  //       color: "green-text-gradient",
-  //     },
-  //     {
-  //       name: "express",
-  //       color: "blue-text-gradient",
-  //     },
-  //     {
-  //       name: "postgresql",
-  //       color: "pink-text-gradient",
-  //     },
-  //     {
-  //       name: "prisma",
-  //       color: "green-text-gradient",
-  //     },
-  //     {
-  //       name: "aws",
-  //       color: "orange-text-gradient",
-  //     },
-  //   ],
-  //   image: jafel,
-  //   source_code_link: "https://github.com/JacceyCode/jira_app",
-  //   live_demo: "https://main.d2mefg2mgkp7t4.amplifyapp.com",
-  // },
+const projects: Project[] = [
+  {
+    name: "OneSchool",
+    description:
+      "A learning management platform that supports structured, collaborative learning. Empowers educators to manage courses and helps learners track their progress.",
+    tags: [
+      "Next.js",
+      "Redux RTK",
+      "Tailwindcss",
+      "Typescript",
+      "Shadcn-ui",
+      "Zod",
+      "Framer Motion",
+      "Clerk Auth",
+      "Node.js",
+      "Express.js",
+      "Stripe",
+      "Vercel",
+      "Docker",
+      "AWS Api Gateway",
+      "AWS Lambda",
+      "AWS DynamoDB",
+      "AWS S3",
+      "AWS ECR",
+      "AWS Cloudfront",
+    ],
+    image: "/images/lms.png",
+    source_code_link: "https://github.com/JacceyCode/lms",
+    live_demo: "https://learning-management-system-blue.vercel.app/",
+  },
   {
     name: "CarePlus+",
     description:
       "A healthcare self-management system that allows users to schedule medical appointments. It addresses issues like long wait times, avoid scheduling conflicts, and eliminate the need for rescheduling due to physician unavailability.",
     tags: [
-      {
-        name: "nextjs",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "typescript",
-        color: "green-text-gradient",
-      },
-      {
-        name: "tailwindcss",
-        color: "pink-text-gradient",
-      },
-      {
-        name: "shadcn",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "appwrite",
-        color: "green-text-gradient",
-      },
-      {
-        name: "sentry",
-        color: "orange-text-gradient",
-      },
-      {
-        name: "twilio",
-        color: "pink-text-gradient",
-      },
+      "Nextjs",
+      "Typescript",
+      "tailwindcss",
+      "Shadcn-ui",
+      "Appwrite",
+      "Sentry",
+      "Twilio",
     ],
     image: "/images/care-plus.png",
     source_code_link: "https://github.com/JacceyCode/careplus",
@@ -472,42 +383,15 @@ const projects = [
     description:
       "A modern app for managing financial transactions and budgets, providing instant access to multiple accounts from one platform.",
     tags: [
-      {
-        name: "nextjs",
-        color: "orange-text-gradient",
-      },
-      {
-        name: "typescript",
-        color: "green-text-gradient",
-      },
-      {
-        name: "tailwindcss",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "shadcn",
-        color: "pink-text-gradient",
-      },
-      {
-        name: "chartjs",
-        color: "green-text-gradient",
-      },
-      {
-        name: "appwrite",
-        color: "pink-text-gradient",
-      },
-      {
-        name: "plaid",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "dwolla",
-        color: "orange-text-gradient",
-      },
-      {
-        name: "sentry",
-        color: "pink-text-gradient",
-      },
+      "Nextjs",
+      "Typescript",
+      "Tailwindcss",
+      "Shadcn-ui",
+      "Chartjs",
+      "Appwrite",
+      "Plaid",
+      "Dwolla",
+      "Sentry",
     ],
     image: "/images/bank.png",
     source_code_link: "https://github.com/JacceyCode/finance",
@@ -517,177 +401,16 @@ const projects = [
     name: "Apple iPhone 15 Pro",
     description:
       "Experience the cutting-edge innovation of the Apple iPhone 15 Pro with a stunning titanium frame and the most advanced A17 Bionic chip for unparalleled performance.",
-    tags: [
-      {
-        name: "react",
-        color: "orange-text-gradient",
-      },
-      {
-        name: "tailwindcss",
-        color: "green-text-gradient",
-      },
-      {
-        name: "gsap",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "react_three_fiber",
-        color: "pink-text-gradient",
-      },
-      {
-        name: "sentry",
-        color: "orange-text-gradient",
-      },
-    ],
+    tags: ["React.js", "Tailwindcss", "GSAP", "React_Three_Fiber", "Sentry"],
     image: "/images/apple.png",
     source_code_link: "https://github.com/JacceyCode/apple",
     live_demo: "https://apple-motion-page.vercel.app",
   },
-  // {
-  //   name: "SeronEstate App",
-  //   description:
-  //     "An estate management app that allows for sale, purchase or rent of apartments with a chat feature for easy communication among users.",
-  //   tags: [
-  //     {
-  //       name: "react",
-  //       color: "orange-text-gradient",
-  //     },
-  //     {
-  //       name: "typescript",
-  //       color: "green-text-gradient",
-  //     },
-  //     {
-  //       name: "zustand",
-  //       color: "blue-text-gradient",
-  //     },
-  //     {
-  //       name: "sass",
-  //       color: "pink-text-gradient",
-  //     },
-  //     {
-  //       name: "leaflet map",
-  //       color: "green-text-gradient",
-  //     },
-  //     {
-  //       name: "cloudinary",
-  //       color: "pink-text-gradient",
-  //     },
-  //     {
-  //       name: "nodejs",
-  //       color: "blue-text-gradient",
-  //     },
-  //     {
-  //       name: "express",
-  //       color: "orange-text-gradient",
-  //     },
-  //     {
-  //       name: "mongodb",
-  //       color: "pink-text-gradient",
-  //     },
-  //     {
-  //       name: "prisma",
-  //       color: "orange-text-gradient",
-  //     },
-  //     {
-  //       name: "jwtoken",
-  //       color: "green-text-gradient",
-  //     },
-  //     {
-  //       name: "socket-io",
-  //       color: "blue-text-gradient",
-  //     },
-  //   ],
-  //   image: seron,
-  //   source_code_link: "https://github.com/JacceyCode/estate-app",
-  //   live_demo: "https://seron-estate-app.vercel.app",
-  // },
-  // {
-  //   name: "Oasis",
-  //   description:
-  //     "An app developed for tourist's to book cabin accommodation of their choice before a scheduled visit to the tourist centre.",
-  //   tags: [
-  //     {
-  //       name: "nextjs",
-  //       color: "orange-text-gradient",
-  //     },
-  //     {
-  //       name: "nextauth",
-  //       color: "green-text-gradient",
-  //     },
-  //     {
-  //       name: "tailwindcss",
-  //       color: "blue-text-gradient",
-  //     },
-  //     {
-  //       name: "supabase",
-  //       color: "pink-text-gradient",
-  //     },
-  //   ],
-  //   image: oasisclt,
-  //   source_code_link:
-  //     "https://github.com/JacceyCode/the-wild-aosis-client-website",
-  //   live_demo: "https://the-wild-aosis-client-website.vercel.app",
-  // },
-  // {
-  //   name: "E-Blog App",
-  //   description:
-  //     "A platform enabling writers to publish creative works for avid readers to engage with and provide valuable feedback.",
-  //   tags: [
-  //     {
-  //       name: "react",
-  //       color: "orange-text-gradient",
-  //     },
-  //     {
-  //       name: "nodejs",
-  //       color: "green-text-gradient",
-  //     },
-  //     {
-  //       name: "express",
-  //       color: "blue-text-gradient",
-  //     },
-  //     {
-  //       name: "mongodb",
-  //       color: "pink-text-gradient",
-  //     },
-  //     {
-  //       name: "jwtoken",
-  //       color: "green-text-gradient",
-  //     },
-  //     {
-  //       name: "css",
-  //       color: "pink-text-gradient",
-  //     },
-  //   ],
-  //   image: blog,
-  //   source_code_link: "https://github.com/JacceyCode/blog-site",
-  //   live_demo: "https://jacceyblog.netlify.app",
-  // },
   {
     name: "D-MarketPlace",
     description:
       "An online e-commerce store incorporating a seamless payment gateway, user account management, authentication, and an intuitive cart system.",
-    tags: [
-      {
-        name: "react",
-        color: "orange-text-gradient",
-      },
-      {
-        name: "redux",
-        color: "green-text-gradient",
-      },
-      {
-        name: "tailwindcss",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "firebase",
-        color: "pink-text-gradient",
-      },
-      {
-        name: "stripe",
-        color: "green-text-gradient",
-      },
-    ],
+    tags: ["React.js", "Redux", "Tailwindcss", "Firebase", "Stripe"],
     image: "/images/dmarketplace.webp",
     source_code_link: "https://github.com/JacceyCode/themarketplace",
     live_demo: "https://dmarketplace.netlify.app",
@@ -696,106 +419,20 @@ const projects = [
     name: "Natours",
     description:
       "A travel application designed and deployed to enthrall travel enthusiasts by showcasing different tourist attractions, facilitating exploration, reservation, and secure payments.",
-    tags: [
-      {
-        name: "nodejs",
-        color: "orange-text-gradient",
-      },
-      {
-        name: "express",
-        color: "green-text-gradient",
-      },
-      {
-        name: "mongodb",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "pug",
-        color: "pink-text-gradient",
-      },
-      {
-        name: "jwtoken",
-        color: "green-text-gradient",
-      },
-      {
-        name: "stripe",
-        color: "pink-text-gradient",
-      },
-    ],
+    tags: ["Nodejs", "Express.js", "MongoDB", "Pug", "JWT", "Stripe"],
     image: "/images/natours.webp",
     source_code_link: "https://github.com/JacceyCode/Natours",
     live_demo: "https://mynatours-3ltq.onrender.com",
   },
-  // {
-  //   name: "Oasis Management App",
-  //   description:
-  //     "An app developed for a resort to strategically prioritize customer management and reservations. With a glance on the app's dashboard, every customer's details and preferences can be gotten at the click of a finger.",
-  //   tags: [
-  //     {
-  //       name: "react",
-  //       color: "orange-text-gradient",
-  //     },
-  //     {
-  //       name: "styled_components",
-  //       color: "green-text-gradient",
-  //     },
-  //     {
-  //       name: "recharts",
-  //       color: "blue-text-gradient",
-  //     },
-  //     {
-  //       name: "supabase",
-  //       color: "pink-text-gradient",
-  //     },
-  //   ],
-  //   image: oasismgt,
-  //   source_code_link: "https://github.com/JacceyCode/the-wild-oasis",
-  //   live_demo: "https://the-wild-oasis-vacation-house.netlify.app",
-  // },
   {
     name: "MovieBox",
     description:
       "A website built for movie lover's to checkout the latest movie trends, see reviews and have a sneak peek of the trailer before hitting the cinema.",
-    tags: [
-      {
-        name: "react",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "tailwindcss",
-        color: "green-text-gradient",
-      },
-      {
-        name: "tmdb_api",
-        color: "pink-text-gradient",
-      },
-    ],
+    tags: ["React.js", "Tailwindcss", "TMDB_API"],
     image: "/images/moviebox.webp",
     source_code_link: "https://github.com/JacceyCode/HNGxStage2Task",
     live_demo: "https://mymoviestreambox.netlify.app",
   },
-  // {
-  //   name: "Pizza Hub",
-  //   description:
-  //     "A website built for pizza lover's, providing a seamless ordering, delivery management and payment experience from the comfort of their homes. This helps to increase the pizza order rate per hour and improve the sales number at least by 40% for the company.",
-  //   tags: [
-  //     {
-  //       name: "react",
-  //       color: "blue-text-gradient",
-  //     },
-  //     {
-  //       name: "redux",
-  //       color: "green-text-gradient",
-  //     },
-  //     {
-  //       name: "tailwindcss",
-  //       color: "pink-text-gradient",
-  //     },
-  //   ],
-  //   image: pizza,
-  //   source_code_link: "https://github.com/JacceyCode/fast-react-pizza",
-  //   live_demo: "https://orderfastpizza.netlify.app",
-  // },
 ];
 
 export {
--- src/sections/Showcase.tsx
+++ src/sections/Showcase.tsx
@@ -1,12 +1,13 @@
 import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { projects } from "../constants";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const Showcase = () => {
+  const [showProjects, setShowProjects] = useState<boolean>(false);
   const sectionRef = useRef(null);
   const project1Ref = useRef(null);
   const project2Ref = useRef(null);
@@ -80,6 +81,18 @@ const Showcase = () => {
               <p className="text-white-50 md:text-xl">
                 {projects[0].description}
               </p>
+
+              {projects[0].tags.map((tag, index) => {
+                return (
+                  <span
+                    key={index}
+                    className="text-blue-50 text-sm italic md:text-lg"
+                  >
+                    {tag}
+                    {index !== projects[0].tags.length - 1 ? " • " : ""}
+                  </span>
+                );
+              })}
             </div>
           </div>
 
@@ -115,14 +128,17 @@ const Showcase = () => {
                 </div>
               </div>
               <h2>{projects[1].name}</h2>
-              <p className="text-white-50 md:text-xl">
-                {projects[1].description.length > 40
-                  ? `${projects[1].description
-                      .split(" ")
-                      .slice(0, 7)
-                      .join(" ")} . . .`
-                  : projects[1].description}
-              </p>
+              {projects[1].tags.map((tag, index) => {
+                return (
+                  <span
+                    key={index}
+                    className="text-blue-50 text-sm italic md:text-lg"
+                  >
+                    {tag}
+                    {index !== projects[1].tags.length - 1 ? " • " : ""}
+                  </span>
+                );
+              })}
             </div>
 
             <div className="project" ref={project3Ref}>
@@ -157,17 +173,113 @@ const Showcase = () => {
               </div>
 
               <h2>{projects[2].name}</h2>
-              <p className="text-white-50 md:text-xl">
-                {projects[2].description.length > 40
-                  ? `${projects[2].description
-                      .split(" ")
-                      .slice(0, 7)
-                      .join(" ")} . . .`
-                  : projects[2].description}
-              </p>
+              {projects[2].tags.map((tag, index) => {
+                return (
+                  <span
+                    key={index}
+                    className="text-blue-50 text-sm italic md:text-lg"
+                  >
+                    {tag}
+                    {index !== projects[2].tags.length - 1 ? " • " : ""}
+                  </span>
+                );
+              })}
             </div>
           </div>
         </div>
+
+        {/* BUTTON */}
+        {!showProjects && (
+          <div className="flex justify-center">
+            <button
+              className="w-80 mt-10"
+              onClick={() => setShowProjects(true)}
+            >
+              <div className="cta-button group">
+                <div className="bg-circle" />
+                <p className="text">{"See More Projects . . ."}</p>
+                <div className="arrow-wrapper">
+                  <img src="/images/arrow-down.svg" alt="arrow" />
+                </div>
+              </div>
+            </button>
+          </div>
+        )}
+
+        {showProjects && (
+          <>
+            <div className="project-list-wrapper !w-full lg:!flex-row flex-wrap mt-10  justify-center">
+              {projects.slice(3).map((project) => (
+                <div
+                  key={project.name}
+                  className="md:w-[45%] lg:w-[30%] project relative shadow-md shadow-cyan-300 rounded-xl"
+                >
+                  <div className="image-wrapper bg-white-50">
+                    <img src={project.image} alt={project.name} />
+                  </div>
+
+                  <div className="absolute top-2 right-2 flex flex-col-reverse items-center gap-2">
+                    <div
+                      onClick={() =>
+                        window.open(project.source_code_link, "_blank")
+                      }
+                      className="black-gradient size-10 rounded-full flex justify-center items-center cursor-pointer"
+                    >
+                      <img
+                        src={"/images/github.png"}
+                        alt="github"
+                        className="size-8 object-contain"
+                      />
+                    </div>
+
+                    <div
+                      onClick={() => window.open(project.live_demo, "_blank")}
+                      className="black-gradient size-10 rounded-full flex justify-center items-center cursor-pointer"
+                    >
+                      <img
+                        src={"/images/link.png"}
+                        alt="live demo"
+                        className="size-8 object-contain"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="p-2 space-y-2">
+                    <h2>{project.name}</h2>
+                    <p className="text-white-50 md:text-xl">
+                      {project.description}
+                    </p>
+                    {project.tags.map((tag, index) => {
+                      return (
+                        <span
+                          key={index}
+                          className="text-blue-50 text-sm italic md:text-lg"
+                        >
+                          {tag}
+                          {index !== project.tags.length - 1 ? " • " : ""}
+                        </span>
+                      );
+                    })}
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="flex justify-center">
+              <button
+                className="w-80 mt-10"
+                onClick={() => setShowProjects(false)}
+              >
+                <div className="cta-button group">
+                  <div className="bg-circle" />
+                  <p className="text">"See Less Projects . . ."</p>
+                  <div className="arrow-wrapper">
+                    <img src="/images/arrow-down.svg" alt="arrow" />
+                  </div>
+                </div>
+              </button>
+            </div>
+          </>
+        )}
       </div>
     </section>
   );
